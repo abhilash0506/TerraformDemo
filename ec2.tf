e82c09205e5c9f0665803a24772b49fa0392dcf9
EC2 Default User <ec2-user@ip-172-31-13-137.ap-south-1.compute.internal>
2021-06-14 13:30:06 +0000

    first commit

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "Terraform" {
+  ami           = "ami-0ad704c126371a549"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServerInstance"
+  }
+}
