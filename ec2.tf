83b98772bd0bb04a38dcc2ec397c2b52d6d40fea
abhilash0506 <77882729+abhilash0506@users.noreply.github.com>
2021-06-15 10:34:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,6 +17,14 @@ provider "aws" {
 resource "aws_instance" "Terraform" {
   ami           = "ami-0ad704c126371a549"
   instance_type = "t2.micro"
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  sudo su
+                  yum -y install httpd
+                  echo "<p> My Instance! </p>" >> /var/www/html/index.html
+                  sudo systemctl enable httpd
+                  sudo systemctl start httpd
+                  EOF
 
   tags = {
     Name = "AppServerInstance"
